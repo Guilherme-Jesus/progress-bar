3378636325e0df3ee6d9f394ad6cbca58e495bc0
Guilherme Jesus <guicoutoo@outlook.com>
2023-10-04 19:58:35 -0300

    [x] adicionado audios e animacoes

---
+++ helpers/helpers.js
@@ -0,0 +1,7 @@
+function getElementById(id) {
+  return document.getElementById(id);
+}
+
+function calculatePercentage() {
+  return (progressData.currentAula / progressData.totalAulas) * 100;
+}
--- js/index.js
+++ js/index.js
@@ -7,59 +7,44 @@ const progressData = {
   currentAula: 0,
 };
 
+// Helpers
 function getElementById(id) {
   return document.getElementById(id);
 }
 
-function updatePercentageLabel() {
+function calculatePercentage() {
+  return (progressData.currentAula / progressData.totalAulas) * 100;
+}
+
+function updateAulaLabel() {
+  const aulaAtualLabel = getElementById("aulaAtual");
+  aulaAtualLabel.innerText = `Aula Atual: ${progressData.currentAula}`;
+}
+
+function updatePercentagePosition() {
   const percentageLabel = getElementById("percentageLabel");
+  const position = calculatePercentage();
   percentageLabel.innerText = progressData.currentAula;
-  const position = (progressData.currentAula / progressData.totalAulas) * 100;
   percentageLabel.style.left = `calc(${position}% - 8px)`;
 }
 
-function updateCurrentAula(step) {
-  progressData.currentAula += step;
-  updateAulaAtualLabel();
+function updateCompletionStatus() {
+  progressData.completed = progressData.currentProgress >= 100;
+  if (progressData.completed) {
+    progressData.currentProgress = 100;
+  }
 }
 
-function calculateProgress() {
-  progressData.currentProgress =
-    (progressData.currentAula / progressData.totalAulas) * 100;
+function incrementCurrentAula(step) {
+  progressData.currentAula += step;
+  progressData.currentProgress = calculatePercentage();
   progressData.previousProgress = Math.max(
     progressData.previousProgress,
     progressData.currentProgress
   );
 }
 
-function updateAulaAtualLabel() {
-  const aulaAtualLabel = getElementById("aulaAtual");
-  aulaAtualLabel.innerText = `Aula Atual: ${progressData.currentAula}`;
-}
-
-function move(step) {
-  if (
-    (step > 0 &&
-      (progressData.currentProgress < 100 || !progressData.completed)) ||
-    (step < 0 && progressData.currentAula > 0)
-  ) {
-    updateCurrentAula(step);
-    calculateProgress();
-    updateCompletionStatus();
-    checkForAchievedMarkers();
-    updateProgressBars();
-  }
-}
-function updateCompletionStatus() {
-  if (progressData.currentProgress >= 100) {
-    progressData.currentProgress = 100;
-    progressData.completed = true;
-    // alert("Parabéns! Você completou todas as etapas.");
-  } else {
-    progressData.completed = false;
-  }
-}
-
+// Progress bars handling
 function fillProgressBar(progressBar, subBarsPerBar, maxProgress, startIndex) {
   progressBar.innerHTML = "";
   for (let j = 0; j < subBarsPerBar; j++) {
@@ -91,20 +76,74 @@ function updateProgressBars() {
     );
   });
 
-  updatePercentageLabel();
+  updatePercentagePosition();
 }
 
+// Markers Handling
+function handleLottieCompletion(index) {
+  stopLottieAudio(index); // ADICIONADO: Parar o áudio.
+
+  const lottieElement = getElementById(`lottie${index}`);
+  lottieElement.style.visibility = "hidden";
+  getElementById(`icon${index}`).style.visibility = "visible";
+}
+function playLottieAudio(index) {
+  const audioElement = getElementById(`lottie${index}Audio`);
+  if (audioElement) {
+    audioElement.play();
+  }
+}
+function stopLottieAudio(index) {
+  const audioElement = getElementById(`lottie${index}Audio`);
+  if (audioElement) {
+    audioElement.pause();
+    audioElement.currentTime = 0; // Reseta o áudio para o início.
+  }
+}
 function checkForAchievedMarkers() {
   [25, 50, 75].forEach((marker, index) => {
     if (
       progressData.currentProgress >= marker &&
       !progressData.achievedMarkers[index]
     ) {
-      // alert(`Você alcançou ${marker}% de progresso!`);
-      getElementById(`icon${index + 1}`).style.visibility = "visible";
+      getElementById(`icon${index + 1}`).style.visibility = "hidden";
+      playLottieAudio(index + 1); // ADICIONADO: Iniciar áudio.
+      const lottieElement = getElementById(`lottie${index + 1}`);      lottieElement.style.visibility = "visible";
+      const lottiePlayer = lottieElement.querySelector("lottie-player");
+      lottiePlayer.play();
+
+      lottiePlayer.addEventListener("complete", () =>
+        handleLottieCompletion(index + 1)
+      );
       progressData.achievedMarkers[index] = true;
     }
   });
+}
+
+function initializeLotties() {
+  for (let i = 1; i <= 3; i++) {
+    const lottieElement = getElementById(`lottie${i}`).querySelector(
+      "lottie-player"
+    );
+    lottieElement.pause();
+    lottieElement.loop = false;
+    lottieElement.autoplay = false;
+  }
+}
+
+// Movement
+function move(step) {
+  const canMoveForward =
+    step > 0 && (progressData.currentProgress < 100 || !progressData.completed);
+  const canMoveBackward = step < 0 && progressData.currentAula > 0;
+
+  if (canMoveForward || canMoveBackward) {
+    incrementCurrentAula(step);
+    updateCompletionStatus();
+    checkForAchievedMarkers();
+    updateProgressBars();
+    updateAulaLabel();
+  }
 }
 
 function moveForward() {
@@ -115,4 +154,7 @@ function moveBackward() {
   move(-1);
 }
 
-updateAulaAtualLabel();
+// Initialize
+initializeLotties();
+updateProgressBars();
+updateAulaLabel();
